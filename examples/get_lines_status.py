11d2011360d1010ea5844fa3a470da1b57f9c500
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2023-02-13 15:17:44 -0800

    remove logging comments

--- examples/get_lines_status.py
+++ examples/get_lines_status.py
@@ -2,23 +2,11 @@
 """Attach a log handler to print logs to stdout."""
 
 from aaopto_aotf.aotf import MPDS
-from aaopto_aotf.device_codes import InputMode, BlankingMode
-import logging
 import pprint
 
-## Send log messages to stdout so we can see every outgoing/incoming tiger msg.
-#class LogFilter(logging.Filter):
-#    def filter(self, record):
-#        return record.name.split(".")[0].lower() in ['aaopto_aotf']
-#
-#fmt = '%(asctime)s.%(msecs)03d %(levelname)s %(name)s: %(message)s'
-#logger = logging.getLogger()
-#logger.setLevel(logging.DEBUG)
-#logger.addHandler(logging.StreamHandler())
-#logger.handlers[-1].setFormatter(logging.Formatter(fmt=fmt))
-#logger.handlers[-1].addFilter(LogFilter())  # Remove parse lib log messages.
+DEV_NAME = '/dev/ttyUSB0'
 
-aotf = MPDS('/dev/ttyUSB0')
+aotf = MPDS(DEV_NAME)
 print(f"Product id: {aotf.get_product_id()}")
 status = aotf.get_lines_status()
 pprint.pprint(status)
